fdedbde67abdd05b99c9ec085cfea7fe67d2b960
teppei667 <teptympei.sasstjgada.666@gmail.com>
2021-10-04 07:25:59 +0000

    finish make end_user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homes#top'
   devise_for :end_users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :end_users, only: [:index, :show, :edit, :update]
+  get 'mypage/:id' => 'end_users#mypage', as: 'mypage'
 end
